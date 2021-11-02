13b0eeb399226bbe111857be03c8438204150d28
DomeSy <397163123@qq.com>
2021-11-02 11:21:17 +0800

    更改

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -15,7 +15,7 @@ const Footer: React.FC = () => (
       {
         key: 'github',
         title: <GithubOutlined />,
-        href: 'https://github.com/ant-design/ant-design-pro',
+        href: 'https://github.com/DomeSy/Ant-Design-Pro-V5',
         blankTarget: true,
       },
       {
